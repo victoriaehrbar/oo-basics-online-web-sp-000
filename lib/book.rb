38583a712b72de8d01c5a4d4f1bf187047d891d4
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-25 15:25:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,4 +16,20 @@ class Book
     @author
   end
 
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
 end
