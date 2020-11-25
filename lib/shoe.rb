8fdb71bd207eb7713c9132a73ea8a4c3db537d1c
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-25 15:28:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :brand
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
